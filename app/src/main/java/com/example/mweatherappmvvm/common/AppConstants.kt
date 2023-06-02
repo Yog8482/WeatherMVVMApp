72ecf1a3345402719c24b5f8b2af577cf3e15092
Yog <developer.yog33@gmail.com>
2023-06-02 18:55:38 +0530

    Location Helper updated, Added App constants for location update interval

--- app/src/main/java/com/example/mweatherappmvvm/common/AppConstants.kt
+++ app/src/main/java/com/example/mweatherappmvvm/common/AppConstants.kt
@@ -2,4 +2,7 @@ package com.example.mweatherappmvvm.common
 
 object AppConstants {
     enum class SEARCH_BY {CITY_NAME, CORDINATES}
+
+    const val LOCATION_UPDATE_INTERVAL =  10000  //millis
+    const val LOCATION_UPDATE_INTERVAL_FASTEST =  5000  //millis
 }
